c90794d69799d3ecc8384097547edac3dbb2efc4
SAYOK-PC\jika <k.jijee@gmail.com>
2021-06-27 04:27:04 +0530

    new commit

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "Developer's Guide",
+  },
+  plugins: [
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
